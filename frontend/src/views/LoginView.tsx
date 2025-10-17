7c1e8c9ee3e2aa63a2d02fcd6ffc9ada6743eefd
Pau Ramón <pau.ramon@qualifyze.com>
2025-10-17 09:16:26 +0200

    feat: create auth layout

--- frontend/src/views/LoginView.tsx
+++ frontend/src/views/LoginView.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 export default function LoginView() {
   return (
     <>
-      <div>LoginView</div>
-
       <nav>
         <Link to="/auth/register">¿No tienes cuenta? Registrate aqui</Link>
       </nav>
--- frontend/src/views/RegisterView.tsx
+++ frontend/src/views/RegisterView.tsx
@@ -3,8 +3,6 @@ import { Link } from "react-router-dom";
 export default function RegisterView() {
   return (
     <>
-      <div>RegisterView</div>
-
       <nav>
         <Link to="/auth/login">¿Ya tienes cuenta? Inicia sesion aqui</Link>
       </nav>
